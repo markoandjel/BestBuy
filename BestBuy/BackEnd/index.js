906561a52f562324140c04ba5c4dcb6aa964ed67
markoandjel <60831775+markoandjel@users.noreply.github.com>
2023-03-27 19:36:36 +0200

    dodavanje inicijalnog fronta

--- BestBuy/BackEnd/index.js
+++ BestBuy/BackEnd/index.js
@@ -4,20 +4,32 @@ const routes_prodavnica=require('./routes/routes_prodavnica')
 const routes_proizvodjac=require('./routes/routes_proizvodjac')
 const routes_korisnik=require('./routes/routes_korisnik')
 const routes_tip_proizvod=require('./routes/routes_tip_proizvod')
+const cors=require('cors')
 require('dotenv').config()
 const mongoString=process.env.DATABASE_URL
 
 const app=express()
+var http=require('http')
 
-app.use(express.json())
-app.use('/',routes_prodavnica)
-app.use('/',routes_proizvodjac)
-app.use('/',routes_korisnik)
-app.use('/',routes_tip_proizvod)
-
-app.listen(3000,()=>{
+http.createServer(app)
+.listen(3000,()=>{
     console.log(`Server Started at ${3000}`)
 })
+
+var corsOptions = {
+    origin: 'http://localhost:5500',
+    optionsSuccessStatus: 200, // For legacy browser support
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json())
+app.use('/',cors(),routes_prodavnica)
+app.use('/',cors(),routes_proizvodjac)
+app.use('/',cors(),routes_korisnik)
+app.use('/',cors(),routes_tip_proizvod)
+
+
 
 mongoose.connect(mongoString)
 const db=mongoose.connection
