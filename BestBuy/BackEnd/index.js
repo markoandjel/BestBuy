c61b7aa169b890b22dd53f64755e5eefb564e636
markoandjel <60831775+markoandjel@users.noreply.github.com>
2023-03-27 19:45:22 +0200

    Ispravka index.js fajla

--- BestBuy/BackEnd/index.js
+++ BestBuy/BackEnd/index.js
@@ -4,6 +4,9 @@ const routes_prodavnica=require('./routes/routes_prodavnica')
 const routes_proizvodjac=require('./routes/routes_proizvodjac')
 const routes_korisnik=require('./routes/routes_korisnik')
 const routes_tip_proizvod=require('./routes/routes_tip_proizvod')
+const routes_proizvod=require('./routes/routes_proizvod')
+const routes_proizvod_prodavnica=require('./routes/routes_proizvod_prodavnica')
+
 const cors=require('cors')
 require('dotenv').config()
 const mongoString=process.env.DATABASE_URL
@@ -15,21 +18,22 @@ http.createServer(app)
 .listen(3000,()=>{
     console.log(`Server Started at ${3000}`)
 })
+app.use(express.json())
+app.use('/',routes_prodavnica)
+app.use('/',routes_proizvodjac)
+app.use('/',routes_korisnik)
+app.use('/',routes_tip_proizvod)
+app.use('/',routes_proizvod)
+app.use('/',routes_proizvod_prodavnica)
 
+
+//CORS
 var corsOptions = {
     origin: 'http://localhost:5500',
     optionsSuccessStatus: 200, // For legacy browser support
 }
 
 app.use(cors(corsOptions));
-
-app.use(express.json())
-app.use('/',cors(),routes_prodavnica)
-app.use('/',cors(),routes_proizvodjac)
-app.use('/',cors(),routes_korisnik)
-app.use('/',cors(),routes_tip_proizvod)
-
-
 
 mongoose.connect(mongoString)
 const db=mongoose.connection
